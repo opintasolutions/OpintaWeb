42ca6f573bcf3217d12e43f57aa4cc2e11288ad7
CoderPrans <pranavbhaskar17@gmail.com>
2019-07-12 22:07:44 +0530

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+exports.onInitialClientRender = () => {
+  ;(function(d, w, c) {
+    w.ChatraID = "duQxLzP2FMBkmQkam"
+    var s = d.createElement("script")
+    w[c] =
+      w[c] ||
+      function() {
+        ;(w[c].q = w[c].q || []).push(arguments)
+      }
+    s.async = true
+    s.src = "https://call.chatra.io/chatra.js"
+    if (d.head) d.head.appendChild(s)
+  })(document, window, "Chatra")
+}
